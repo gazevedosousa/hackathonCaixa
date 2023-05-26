bd6432a718fc650d5a6f33352e77c36f339ec01e
Gabriel <gazevedosousa@hotmail.com>
2023-05-26 12:03:47 -0300

    commit inicial

---
+++ src/main/java/com/hackaton/simulacao/SimulacaoApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.simulacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/simulacao/SimulacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.simulacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
